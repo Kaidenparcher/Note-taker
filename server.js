33f72cc922ea179abb8d755879618ea1d1e3475f
Kaiden Parcher <ktparcher@.com>
2023-04-27 09:59:29 -0700

    HTML get routes created"

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 
-const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+const app = express();
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+// HTML route for notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+// HTML route for base index
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Listen command for displaying on Port.
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
